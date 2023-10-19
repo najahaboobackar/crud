4a2705399da4f38e45a46154f6be2eec1e415e30
najah <najahaboobacker79@gmail.com>
2023-10-19 18:10:27 +0530

    3

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
+
 const router =express.Router();
 const users=[{
     fisrtName:"Jon",
@@ -11,4 +14,12 @@ router.get("/",(req,res)=>{
     console.log(users)
     res.send("heello")
 });
+router.post("/",(req,res)=>{
+    const user=req.body;
+    const userid=uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+    const userWithid={... user,id :userid}
+   
+   users.push(user);
+    res.send(`user to add ${user.fisrtName} added to database`);
+})
 export default router;
