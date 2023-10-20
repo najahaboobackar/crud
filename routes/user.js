471a42ef85b1618c831724638cc1e225eb802919
najah <najahaboobacker79@gmail.com>
2023-10-21 02:55:01 +0530

    5

--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,8 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 const router =express.Router();
-const users=[{
-    fisrtName:"Jon",
-    lastName:"doe",
-    age:"40"
-
-}]
+const users=[
+    ];
 // all routing starts with / userss
 router.get("/",(req,res)=>{
     console.log(users)
@@ -18,8 +14,12 @@ router.post("/",(req,res)=>{
     const user=req.body;
     const userid=uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
     const userWithid={... user,id :userid}
-   
-   users.push(user);
-    res.send(`user to add ${user.fisrtName} added to database`);
+     users.push(userWithid);
+    res.send(`user to add ${user.firstName} added to database`);
+})
+router.get('/:id',(req,res)=>{
+    const {id }=req.params;
+    const found=users.find((user)=>user.id===id)
+    res.send(found);
 })
 export default router;
