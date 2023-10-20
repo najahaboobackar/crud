79db7df62f78dbc26f511e9e13baa0d97f26f799
najah <najahaboobacker79@gmail.com>
2023-10-21 03:09:25 +0530

    34

--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,11 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 const router =express.Router();
-const users=[
+let users=[
     ];
 // all routing starts with / userss
 router.get("/",(req,res)=>{
-    console.log(users)
-    res.send("heello")
+   res.send(users)
 });
 router.post("/",(req,res)=>{
     const user=req.body;
@@ -21,5 +20,11 @@ router.get('/:id',(req,res)=>{
     const {id }=req.params;
     const found=users.find((user)=>user.id===id)
     res.send(found);
+})
+router.delete('/:id',(res,req)=>{
+    const {id }=req.params;
+    users=users.filter((user)=>user.id!==id)
+    res.send(`user withthe i${id}is deleted`)
+
 })
 export default router;
